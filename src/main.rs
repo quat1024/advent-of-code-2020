fe70f6e7581b1a8866022c4060f4f0ae0504c41b
quat1024 <quat1024@users.noreply.github.com>
2020-12-01 00:18:22 -0500

    1a

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,23 @@ fn main() -> io::Result<()> {
 }
 
 fn challenge1a() -> io::Result<()> {
+    //Load file
+    let file_string = fs::read_to_string("1a.txt")?;
+    
+    //Parse file
+    let numbers: Vec<i32> = file_string
+        .split_whitespace()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    
+    //Solve challenge
+    for (index, n1) in numbers.iter().enumerate() {
+        for n2 in numbers[index..].iter() {
+            if n1 + n2 == 2020 {
+                println!("product is {}", n1 * n2);
+            }
+        }
+    }
+    
     Ok(())
 }
