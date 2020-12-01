e70a1306388a12be81f150eda339d3ae85fc1e5e
quat1024 <quat1024@users.noreply.github.com>
2020-12-01 00:21:16 -0500

    1b

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() -> io::Result<()> {
     
     match challenge_name.as_ref() {
         "1a" => challenge1a(),
+        "1b" => challenge1b(),
         unknown => panic!("unknown challenge \"{}\"", unknown)
     }
 }
@@ -26,6 +27,30 @@ fn challenge1a() -> io::Result<()> {
         for n2 in numbers[index..].iter() {
             if n1 + n2 == 2020 {
                 println!("product is {}", n1 * n2);
+            }
+        }
+    }
+    
+    Ok(())
+}
+
+fn challenge1b() -> io::Result<()> {
+    //Load file
+    let file_string = fs::read_to_string("1a.txt")?;
+    
+    //Parse file
+    let numbers: Vec<i32> = file_string
+        .split_whitespace()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    
+    //TODO: This doesn't seem correct, it returns the result 3 times lol
+    for (index1, n1) in numbers.iter().enumerate() {
+        for (index2, n2) in numbers[index1 + 1..].iter().enumerate() {
+            for n3 in numbers[index2 + 1..].iter() {
+                if (n1 + n2 + n3) == 2020 {
+                    println!("product is {}", (n1 * n2 * n3));
+                } 
             }
         }
     }
