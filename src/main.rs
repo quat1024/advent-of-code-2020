b6cb02281ad1b6fede41e885bd15053de6df6530
quat1024 <quat1024@users.noreply.github.com>
2020-12-02 00:29:44 -0500

    2b

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() -> io::Result<()> {
         "1a" => challenge1a(),
         "1b" => challenge1b(),
         "2a" => challenge2a(),
+        "2b" => challenge2b(),
         unknown => panic!("unknown challenge \"{}\"", unknown)
     }
 }
@@ -46,18 +47,16 @@ fn challenge1b() -> io::Result<()> {
     Ok(())
 }
 
-fn challenge2a() -> io::Result<()> {
-    let input = fs::read_to_string("2a.txt")?;
-    
-    #[derive(Debug)]
-    struct Entry {
-        min: i32,
-        max: i32,
-        letter: char,
-        pass: String
-    }
-    
-    let entries: Vec<Entry> = input.lines().map(|line| -> Entry {
+#[derive(Debug)]
+struct PasswordEntry {
+    min: i32,
+    max: i32,
+    letter: char,
+    pass: String
+}
+
+fn parsePasswords<C>(input: String) -> C where C: std::iter::FromIterator<PasswordEntry> {
+    input.lines().map(|line| -> PasswordEntry {
         //Oh no it sucks
         let line_split: Vec<&str> = line.split(' ').collect();
         let policy_split: Vec<i32> = line_split.get(0).expect("policy").split('-').map(|x| x.parse().expect("number in policy")).collect();
@@ -66,12 +65,33 @@ fn challenge2a() -> io::Result<()> {
         let letter = line_split.get(1).expect("letter").chars().next().expect("a char");
         let pass = line_split.get(2).expect("password").to_string();
         
-        Entry{min, max, letter, pass}
-    }).collect();
+        PasswordEntry{min, max, letter, pass}
+    }).collect()
+}
+
+fn challenge2a() -> io::Result<()> {
+    let input = fs::read_to_string("2a.txt")?;
+    let entries: Vec<PasswordEntry> = parsePasswords(input);
     
     let count = entries.iter().filter(|entry| -> bool {
         let letter_count = entry.pass.chars().filter(|c| *c == entry.letter).count() as i32;
         letter_count >= entry.min && letter_count <= entry.max
+    }).count();
+    
+    println!("{}", count);
+    
+    Ok(())
+}
+
+//wrong: 407 (off by one)
+//wrong: 423 (off by two)
+fn challenge2b() -> io::Result<()> {
+    let input = fs::read_to_string("2a.txt")?;
+    let entries: Vec<PasswordEntry> = parsePasswords(input);
+    
+    let count = entries.iter().filter(|entry| -> bool {
+        let indexable_pass: Vec<char> = entry.pass.chars().collect();
+        indexable_pass.get(entry.min as usize - 1).map(|c| *c == entry.letter).unwrap() ^ indexable_pass.get(entry.max as usize - 1).map(|c| *c == entry.letter).unwrap()
     }).count();
     
     println!("{}", count);
