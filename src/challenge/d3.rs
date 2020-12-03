bf3ff2c5fe708cb965aeb5c064d87614125c9d82
quat1024 <quat1024@users.noreply.github.com>
2020-12-03 00:39:24 -0500

    cargo fmt

--- src/challenge/d3.rs
+++ src/challenge/d3.rs
@@ -5,7 +5,7 @@ use crate::*;
 pub struct Original;
 
 struct Map {
-    trees: Vec<Vec<Entry>> //row-major; are there any better structures available? (yes)
+    trees: Vec<Vec<Entry>>, //row-major; are there any better structures available? (yes)
 }
 
 impl Map {
@@ -13,21 +13,23 @@ impl Map {
         let row = self.trees.get(row)?;
         Some(row[column % row.len()])
     }
-    
+
     fn count_along_slope(&self, rowd: usize, cold: usize) -> usize {
         //This smells like something an iterator can do better, lol
         let mut tree_count = 0;
         let mut row = 0;
         let mut col = 0;
-        
+
         let mut op: Option<Entry> = Some(Entry::Air);
         while let Some(e) = op {
-            if e == Entry::Tree { tree_count += 1; }
+            if e == Entry::Tree {
+                tree_count += 1;
+            }
             row += rowd;
             col += cold;
             op = self.get(row, col);
         }
-        
+
         tree_count
     }
 }
@@ -36,7 +38,10 @@ impl Map {
 impl From<String> for Map {
     fn from(input: String) -> Self {
         Map {
-            trees: input.lines().map(|x| x.chars().map(|c| Entry::from(c)).collect()).collect()
+            trees: input
+                .lines()
+                .map(|x| x.chars().map(|c| Entry::from(c)).collect())
+                .collect(),
         }
     }
 }
@@ -44,7 +49,7 @@ impl From<String> for Map {
 #[derive(Eq, PartialEq, Copy, Clone)]
 enum Entry {
     Air,
-    Tree
+    Tree,
 }
 
 //TODO this is WRONG, use TryFrom
@@ -53,7 +58,7 @@ impl From<char> for Entry {
         match c {
             '.' => Entry::Air,
             '#' => Entry::Tree,
-            etc => panic!("we have TryFrom at home... unknown char {}", etc)
+            etc => panic!("we have TryFrom at home... unknown char {}", etc),
         }
     }
 }
@@ -69,7 +74,11 @@ impl Challenge for Original {
 
     fn part_b(&self, input: String) -> Result<String, ChallengeErr> {
         let map: Map = input.into();
-        
-        Ok([(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)].iter().map(|(rowd, cold)| map.count_along_slope(*rowd, *cold)).product::<usize>().to_string())
+
+        Ok([(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+            .iter()
+            .map(|(rowd, cold)| map.count_along_slope(*rowd, *cold))
+            .product::<usize>()
+            .to_string())
     }
 }
