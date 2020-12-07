7258abc4d85661fc86f0fe4c3521c9a3dd9d2abf
quat1024 <quat1024@users.noreply.github.com>
2020-12-07 01:36:40 -0500

    cargo fmt

--- src/challenge/d7.rs
+++ src/challenge/d7.rs
@@ -1,5 +1,5 @@
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
-use std::collections::{HashSet, HashMap};
 
 use crate::*;
 use regex::Regex;
@@ -12,12 +12,12 @@ lazy_static! {
 
 struct Rule {
     name: String,
-    contents: Vec<ContentsRule>
+    contents: Vec<ContentsRule>,
 }
 
 struct ContentsRule {
     qty: usize,
-    color: String
+    color: String,
 }
 
 impl FromStr for Rule {
@@ -26,16 +26,16 @@ impl FromStr for Rule {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         //As usual TODO for the error handling being absolute garbo
         let mut spaced = s.split(' ');
-        
+
         //The original intention was to grab a slice into the str that contains just the bag name
         //But I dunno how lol
         //Ran into lifetime issues as well later on
         let name = spaced.next().unwrap().to_string() + " " + spaced.next().unwrap();
         assert_eq!(spaced.next(), "bags".into());
         assert_eq!(spaced.next(), "contain".into());
-        
+
         let mut contents: Vec<ContentsRule> = Vec::new();
-        
+
         while let Some(s) = spaced.next() {
             if s == "no" {
                 assert_eq!(spaced.next(), Some("other".into()));
@@ -43,16 +43,16 @@ impl FromStr for Rule {
                 assert_eq!(spaced.next(), None);
                 break;
             }
-            
+
             if let Ok(qty) = s.parse::<usize>() {
                 let color = spaced.next().unwrap().to_string() + " " + spaced.next().unwrap();
-                contents.push(ContentsRule{qty, color});
-                
+                contents.push(ContentsRule { qty, color });
+
                 let _bags = spaced.next(); //"bag," "bags," "bag.", or "bags."
             }
         }
-        
-        Ok(Rule{name, contents})
+
+        Ok(Rule { name, contents })
     }
 }
 
@@ -63,28 +63,28 @@ impl Challenge for Challenge7 {
 
     fn part_a(&self, input: String) -> Result<String, ChallengeErr> {
         let rules: Vec<Rule> = input.lines().map(|s| Rule::from_str(&s).unwrap()).collect();
-        
+
         let reverse_lookup = |name: &str| -> Vec<&str> {
             let mut containers: Vec<&str> = Vec::new();
-            
+
             for rule in rules.iter() {
                 if rule.contents.iter().any(|c| c.color == name) {
                     containers.push(&rule.name);
                 }
             }
-            
+
             containers
         };
-        
+
         let mut to_lookup: HashSet<&str> = HashSet::new();
         let mut found: HashSet<&str> = HashSet::new();
-        
+
         to_lookup.insert("shiny gold".into());
-        
+
         while !to_lookup.is_empty() {
             let to_lookup_clone = to_lookup.clone();
             to_lookup.clear();
-            
+
             for name in to_lookup_clone {
                 for found_name in reverse_lookup(name).iter() {
                     if !found.contains(found_name) {
@@ -94,7 +94,7 @@ impl Challenge for Challenge7 {
                 }
             }
         }
-        
+
         Ok(found.len().to_string())
     }
 
@@ -104,30 +104,35 @@ impl Challenge for Challenge7 {
             .map(|s| Rule::from_str(&s).unwrap())
             .map(|r| (r.name.clone(), r)) //WHEN IN DOUBT JUST CLONE EVERYTHING
             .collect();
-            
+
         let mut cache: HashMap<String, usize> = HashMap::new();
-        
+
         Ok(count_bags_inside(&rules, &mut cache, "shiny gold".into()).to_string())
     }
 }
 
-fn count_bags_inside(rules: &HashMap<String, Rule>, cache: &mut HashMap<String, usize>, bag: String) -> usize {
+fn count_bags_inside(
+    rules: &HashMap<String, Rule>,
+    cache: &mut HashMap<String, usize>,
+    bag: String,
+) -> usize {
     if cache.contains_key(&bag) {
         return *cache.get(&bag).unwrap();
     }
-    
+
     println!("looking up {}", bag);
-    
+
     let entry = rules.get(&bag);
     match entry {
         None => {
             println!("unknown bag {}", bag);
-            return 0
-        },
+            return 0;
+        }
         Some(rule) => {
             let mut sum = 0;
             for contents_rule in rule.contents.iter() {
-               sum += contents_rule.qty * (1 + count_bags_inside(rules, cache, contents_rule.color.clone()))
+                sum += contents_rule.qty
+                    * (1 + count_bags_inside(rules, cache, contents_rule.color.clone()))
             }
             println!("{} can hold {}", bag, sum);
             cache.insert(bag, sum);
