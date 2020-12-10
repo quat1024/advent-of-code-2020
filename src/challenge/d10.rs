286e1287cec11331b77bb118525425e66bd33c43
quat1024 <quat1024@users.noreply.github.com>
2020-12-10 00:08:35 -0500

    10a

--- src/challenge/d10.rs
+++ src/challenge/d10.rs
@@ -7,8 +7,16 @@ impl Challenge for Challenge10 {
         "10a.txt"
     }
 
-    fn part_a(&self, _input: String) -> Result<String, ChallengeErr> {
-        Err(ChallengeErr::NotYetImplemented())
+    fn part_a(&self, input: String) -> Result<String, ChallengeErr> {
+        let mut nums = input.lines().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+        nums.push(0);
+        nums.push(nums.iter().max().unwrap() + 3);
+        nums.sort();
+        
+        let one = nums.array_windows::<2>().filter(|&[a, b]| *b - *a == 1).count();
+        let three = nums.array_windows::<2>().filter(|&[a, b]| *b - *a == 3).count();
+        
+        Ok((one * three).to_string())
     }
 
     fn part_b(&self, _input: String) -> Result<String, ChallengeErr> {
