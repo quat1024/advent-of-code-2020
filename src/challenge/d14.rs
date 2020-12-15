2ea1e8c55bdf0495aa8bd41b7c21df04289019da
quat1024 <quat1024@users.noreply.github.com>
2020-12-14 23:56:34 -0500

    untested unfinshed d14

--- src/challenge/d14.rs
+++ src/challenge/d14.rs
@@ -41,8 +41,48 @@ impl Challenge for Challenge14 {
         Ok(mem.values().sum::<u64>().to_string())
     }
 
-    fn part_b(&self, _input: String) -> Result<String, ChallengeErr> {
-        //Good night!
+    fn part_b(&self, input: String) -> Result<String, ChallengeErr> {
+        //STILL doesn't work, i'm busy on day 15 rn :)
+        
+        
+        
+        let mut mem: HashMap<u64, u64> = HashMap::new();
+        
+        enum MaskBit {
+            Passthrough,
+            One,
+            Floating
+        }
+        
+        //left side of this thing is the high bits
+        let mut mask: Vec<MaskBit> = Vec::with_capacity(36);
+        
+        for line in input.lines() {
+            if let Some(mask_str) = line.strip_prefix("mask = ") {
+                mask.clear();
+                mask.extend(mask_str.chars().map(|ch| match ch {
+                    '0' => MaskBit::Passthrough,
+                    '1' => MaskBit::One,
+                    'X' => MaskBit::Floating,
+                    _ => panic!()
+                }));
+                assert_eq!(mask.len(), 36);
+            } else if let Some(rest) = line.strip_prefix("mem["){
+                let mut split = rest.split("] = ");
+                let base_addr = split.next().unwrap().parse::<u64>().unwrap();
+                let value = split.next().unwrap().parse::<u64>().unwrap();
+                
+                let mut unfloating_addr: u64 = 0;
+                for idx in 0..=35 {
+                    match mask[idx] {
+                        MaskBit::Passthrough => unfloating_addr |= ((1 << 35) >> idx) & base_addr,
+                        MaskBit::One => unfloating_addr |= (1 << 35) >> idx,
+                        _ => ()
+                    }
+                }
+            }
+        }
+        
         unimplemented!()
     }
 }
